de6b88568b2e97f40092bb56622e1e2248b7fe81
thando mini <tzone85@gmail.com>
2019-07-26 01:06:54 +0200

    added a new sqlite database, then made some migrations, used tinker to add come customers and persist them onto the db. Ran a few migrations

--- app/Http/Controllers/CustomersController.php
+++ app/Http/Controllers/CustomersController.php
@@ -2,18 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use \App\Customer;
 use Illuminate\Http\Request;
 
 class CustomersController extends Controller
 {
     public function list() {
 
-    	$customers = [
-			'Thand Mini',
-			'Mncedi Mhla',
-			'Dishman Sombolo',
-			'Another Name'
-		];
+    	$customers = Customer::all();
 
 		return view('internals.customers', [
 			'customers' => $customers
