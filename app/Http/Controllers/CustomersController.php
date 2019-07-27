77552b3ec89a3cc202c4cc48661d0a2d0fb70215
thando mini <tzone85@gmail.com>
2019-07-27 15:41:10 +0200

    reversed migration, then added email field and migrated customer table again, added the email to the form with falidation and made use of the old fxn for holding onto the input of already submitted data should there be an error. Then did added validation

--- app/Http/Controllers/CustomersController.php
+++ app/Http/Controllers/CustomersController.php
@@ -18,8 +18,15 @@ class CustomersController extends Controller
 
 	public function store() {
 
+    	// add validation
+		$data = request()->validate([
+			'name' => 'required|alpha|min:3',
+			'email' => 'email|required|min:3|distinct'
+		]);
+
     	$customer = new Customer();
     	$customer->name = request('name');
+    	$customer->email = request('email');
     	$customer->save();
 
     	return back();
--- resources/views/internals/customers.blade.php
+++ resources/views/internals/customers.blade.php
@@ -4,8 +4,18 @@
     <h1>Customers</h1>
 
     <form action="customers" method="post" class="pb-5">
+
+        <p>Name:</p>
+        <div class="input-group pb-2">
+            <input type="text" name="name" value="{{ old('name') }}">
+            {{ $errors->first('name') }}
+        </div>
+
+        <p>Email:</p>
         <div class="input-group">
-            <input type="text" name="name">
+
+            <input type="email" name="email" value="{{ old('email') }}">
+            {{ $errors->first('email') }}
         </div>
 
         <button type="submit">Add Customer</button>
@@ -15,7 +25,7 @@
     
     <ul>
         @foreach($customers as $customer)
-            <li>{{ $customer->name }}</li>
+            <li>{{ $customer->name }} <span class="text-muted"> ({{ $customer->email }}) </span></li>
         @endforeach
     </ul>
 @endsection
