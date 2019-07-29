771fdb6ba1e8f0c089ee6d6e12ad6b334599bda4
thando mini <tzone85@gmail.com>
2019-07-29 19:39:15 +0200

    used guard instead of fillable, and explained in the docblock the difference between the two

--- app/Customer.php
+++ app/Customer.php
@@ -7,10 +7,20 @@ use Illuminate\Database\Eloquent\Model;
 class Customer extends Model
 {
 	/**
+	 * Fillable Example
 	 * specifying the fields that can be mass assigned
 	 * @var array
 	 */
-	protected $fillable = ['name', 'email', 'active', 'age', 'jobTitle'];
+	//protected $fillable = ['name', 'email', 'active', 'age', 'jobTitle'];
+
+	/**
+	 * Guarded Example
+	 * if array is empty, nothing is guarded. (because we'll be using best practices and validating our input fields, and be weary about what goes into the create method)
+	 * if we put say name, in there, thus name, cannot be mass assigned.
+	 * so Guarded is the opposite of Fillable
+	 * @var array
+	 */
+	protected $guarded = [];
 
 	/**
 	 * scope function for active customers
